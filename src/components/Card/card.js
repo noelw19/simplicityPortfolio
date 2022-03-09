370af3007bbd58f629d03e8ad0ae3ff508280035
noelw19 <noelw19@outlook.com>
2022-03-10 01:01:40 +1300

    added card class and fixed home size

--- src/components/Card/card.js
+++ src/components/Card/card.js
@@ -1,30 +1,57 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import styles from './card.module.css';
+import './index.css'
 
 
-export const ProjCard = ({name, tech, desc, url, image}) => {
-    let techStack = tech.join(', ');
+export class ProjCard extends React.Component {
+    constructor({name, tech, desc, url, image}) {
+        super();
+        this.name = name;
+        this.tech = tech.join(', ');
+        this.desc = desc;
+        this.url = url;
+        this.img = image;
+    }
 
-    // useEffect(() => {
-    // let cardParent = document.querySelector('#cardFather');
-    // cardParent.addEventListener('click', (e) => {
-    //     //     console.log(e.target)
-    //         window.open(url, '_blank');
+    // componentDidMount() {
+    //     let cardParent = document.querySelector('.onclickCard');
+    //     cardParent.addEventListener('click', (e) => {
+    //         console.log(e.target);
+    //         window.open(this.url, '_blank');
     //     })
-    // }, []);
-    // let cardParent = document.querySelector('#cardFather');
-    // cardParent.addEventListener('click', (e) => {
-    //     console.log(e.target)
-    //     // window.open(url, '_blank');
-    // })
-    
-    return (
-        <div className={styles.cardParent} id='cardFather'>
-            <h3>{name}</h3>
-            <img src={image} alt='project view'></img>
-            <div className={styles.flex}><p style={{color: 'var(--orange)', paddingRight: '5px'}}>Tech: </p><p className={styles.cardValText}> {techStack}</p></div>
-            <div className={styles.flex}><p className={styles.cardValText}> {desc}</p></div>
-            
-        </div>
-    )
+    // }
+
+    render() {
+        return (
+            <div className='onclickCard cardParent' id='cardFather' onClick={() => {window.open(this.url, '_blank');}}>
+                <h3>{this.name}</h3>
+                <img src={this.img} alt='project view'></img>
+                <div className='flex'><p style={{color: 'var(--orange)', paddingRight: '5px'}}>Tech: </p><p className='cardValText'> {this.tech}</p></div>
+                <div className='flex'><p className='cardValText'> {this.desc}</p></div>
+            </div>
+        )
+    }
 }
+
+// export const ProjCard = ({name, tech, desc, url, image}) => {
+//     let techStack = tech.join(', ');
+
+//     useEffect(() => {
+//         let cardParent = document.querySelector('.onclickCard');
+//         cardParent.addEventListener('click', (e) => {
+//             console.log(e.target)
+//             window.open(url, '_blank');
+//         })
+//     }, [url]);
+    
+    
+//     return (
+//         <div className='onclickCard cardParent' id='cardFather'>
+//             <h3>{name}</h3>
+//             <img src={image} alt='project view'></img>
+//             <div className='flex'><p style={{color: 'var(--orange)', paddingRight: '5px'}}>Tech: </p><p className='cardValText'> {techStack}</p></div>
+//             <div className='flex'><p className='cardValText'> {desc}</p></div>
+            
+//         </div>
+//     )
+// }
--- src/views/Projects/projects.js
+++ src/views/Projects/projects.js
@@ -17,7 +17,7 @@ export const Projects = () => {
             <p>Click the project to view the site.</p>
             <div className={styles.contentContainer}>
                 <ProjCard name='Cocktail Recipe Finder' tech={['Js', 'React', 'fetch']} url='https://suspicious-northcutt-b99321.netlify.app/' image={cocktailRecipe}></ProjCard>
-                <ProjCard name='Modern Calculator' tech={['Js', 'React', 'Regex']} url='www.validUrl.com' image={calculatorProj}></ProjCard>
+                <ProjCard name='Modern Calculator' tech={['Js', 'React', 'Regex']} url='https://dreamy-yalow-484b54.netlify.app/' image={calculatorProj}></ProjCard>
                 <ProjCard name='Herbal Therapy' tech={['Js', 'React']} desc={aromatherapy} url='https://keen-poitras-6360a4.netlify.app/' image={aromatherapySite}></ProjCard>
                 <ProjCard name='Previous Portfolio' tech={['Js', 'React']} desc={oldPortDesc} url='https://hungry-benz-7536cf.netlify.app/' image={oldPortfolio}></ProjCard>
 

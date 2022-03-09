32546da61b3e5c623622fef868dc9fc69bf1da92
noelw19 <noelw19@outlook.com>
2022-03-09 23:06:09 +1300

    added projects

--- src/components/Card/card.js
+++ src/components/Card/card.js
@@ -1,15 +1,30 @@
+import React, {useEffect} from 'react';
 import styles from './card.module.css';
+
 
 export const ProjCard = ({name, tech, desc, url, image}) => {
     let techStack = tech.join(', ');
+
+    // useEffect(() => {
+    // let cardParent = document.querySelector('#cardFather');
+    // cardParent.addEventListener('click', (e) => {
+    //     //     console.log(e.target)
+    //         window.open(url, '_blank');
+    //     })
+    // }, []);
+    // let cardParent = document.querySelector('#cardFather');
+    // cardParent.addEventListener('click', (e) => {
+    //     console.log(e.target)
+    //     // window.open(url, '_blank');
+    // })
     
     return (
-        <div className={styles.cardParent}>
+        <div className={styles.cardParent} id='cardFather'>
             <h3>{name}</h3>
             <img src={image} alt='project view'></img>
             <div className={styles.flex}><p style={{color: 'var(--orange)', paddingRight: '5px'}}>Tech: </p><p className={styles.cardValText}> {techStack}</p></div>
-            <div className={styles.flex}><p style={{color: 'var(--orange)', paddingRight: '5px'}}>About: </p><p className={styles.cardValText}> {desc}</p></div>
-            <div className={styles.flex}><p style={{color: 'var(--orange)', paddingRight: '5px'}}>URL: </p><p className={styles.cardValText}> {url}</p></div>
+            <div className={styles.flex}><p className={styles.cardValText}> {desc}</p></div>
+            
         </div>
     )
 }
--- src/views/Projects/projects.js
+++ src/views/Projects/projects.js
@@ -1,16 +1,25 @@
 import styles from './proj.module.css';
 import { ProjCard } from '../../components/Card/card';
-import img from '../../views/Home/image.jpeg'
+import cocktailRecipe from '../../images/projects/cocktailRecipe.png'
+import calculatorProj from '../../images/projects/calc.png';
+import aromatherapySite from '../../images/projects/aromatherapy.png';
+import oldPortfolio from '../../images/projects/oldPortfolio.png'
 
 export const Projects = () => {
+
+    let oldPortDesc = 'An older portfolio i built using react, hooks and a few css keyframes for animation, although by the time I was nearing the end of the project I realised i wanted a simple portfolio, something clean and minimalistic instead of a portfolio cluttered with all things i wanted to show I could do.'
+    let aromatherapy = 'An aromatherapy site that i built with hopes of '
+
+
     return (
         <div className='routedPageContainer flexCenter'>
             <h1>Project Board</h1>
+            <p>Click the project to view the site.</p>
             <div className={styles.contentContainer}>
-                <ProjCard name='Project Name' tech={['Js', 'React']} url='www.validUrl.com' image={img}></ProjCard>
-                <ProjCard name='Project Name' tech={['Js', 'React']} url='www.validUrl.com' image={img}></ProjCard>
-                <ProjCard name='Project Name' tech={['Js', 'React']} url='www.validUrl.com' image={img}></ProjCard>
-                <ProjCard name='Project Name' tech={['Js', 'React']} url='www.validUrl.com' image={img}></ProjCard>
+                <ProjCard name='Cocktail Recipe Finder' tech={['Js', 'React', 'fetch']} url='https://suspicious-northcutt-b99321.netlify.app/' image={cocktailRecipe}></ProjCard>
+                <ProjCard name='Modern Calculator' tech={['Js', 'React', 'Regex']} url='www.validUrl.com' image={calculatorProj}></ProjCard>
+                <ProjCard name='Herbal Therapy' tech={['Js', 'React']} desc={aromatherapy} url='https://keen-poitras-6360a4.netlify.app/' image={aromatherapySite}></ProjCard>
+                <ProjCard name='Previous Portfolio' tech={['Js', 'React']} desc={oldPortDesc} url='https://hungry-benz-7536cf.netlify.app/' image={oldPortfolio}></ProjCard>
 
             </div>
         </div>
